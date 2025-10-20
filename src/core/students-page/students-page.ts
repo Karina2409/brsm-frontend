f3c0eb136150e1e89cf1fb309913330867403d83
Karina Kupryianava <karina.kupryianava@gmail.com>
2025-10-20 22:43:45 +0300

    feat: add faculties filter to students page

--- src/core/students-page/students-page.ts
+++ src/core/students-page/students-page.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
 import {Search} from '@components/search';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {FormsModule} from '@angular/forms';
+import {Faculty} from '@interfaces/faculty';
+import {StudentSearchParams} from '@interfaces/student-search-params';
 
 @Component({
   selector: 'app-students-page',
   imports: [
-    Search
+    Search,
+    MultiSelectModule,
+    FormsModule
   ],
   templateUrl: './students-page.html',
   styleUrl: './students-page.scss'
 })
 export class StudentsPage {
+  faculties: Faculty[] = [
+    {name: "ФКП", code: "FKP"},
+    {name: "ФИТУ", code: "FITU"},
+    {name: "ФРЭ", code: "FRE"},
+    {name: "ФКСиС", code: "FKSIS"},
+    {name: "ФИБ", code: "FIB"},
+    {name: "ИЭФ", code: "IEF"},
+    {name: "ВФ", code: "VF"}
+  ];
+
+  filters: StudentSearchParams = {
+    surname: '',
+    faculties: []
+  };
+
   onSearch (surname: string) {
-    console.log(surname);
+    this.filters.surname = surname;
+    this.performSearch();
+  }
+
+  onFacultyChange() {
+    this.performSearch();
+  }
+
+  performSearch() {
+    console.log('Поиск с параметрами:', this.filters);
+    //TODO (сервер): после добавления сервера добавить обращение к сервису
+    // this.studentService.search(this.filters).subscribe(...)
   }
 }
---
+++ src/shared/interfaces/student-search-params.ts
@@ -0,0 +1,4 @@
+export interface StudentSearchParams {
+  surname?: string;
+  faculties?: string[];
+}
